37954a81d160c5c01e7e6a0d4f552e4365ebd52b
lich0079 <lich0079@a599a92a-72a9-11de-90dc-5d84f995b3ea>
2010-12-23 15:23:41 +0000

    init

---
+++ sc2m/starcraft2_manual/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  starcraft2 manual
+//
+//  Created by lich0079 on 10-8-4.
+//  Copyright ibm 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
